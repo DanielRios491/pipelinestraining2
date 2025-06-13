1ed53145336662af2f79acd7d8b7e796146a18f0
DanielRiosLopez <Daniel.Rios@jala.university>
2025-06-13 17:04:00 -0400

    changes on the file postmanNeuman_commands

--- utils/postmanNeuman_commands.sh
+++ utils/postmanNeuman_commands.sh
@@ -9,6 +9,6 @@ restappIp=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{
 # Run Postman collection with newman
 docker run --rm \
     --add-host "restapp:$restappIp" \
-    -v "$(pwd)/tests:/etc/newman" \
+    -v "$(pwd)/utils/tests:/etc/newman" \
     postman/newman run "tests/collectionRestAppTest.json" 
     # --environment "tests/env.json"
