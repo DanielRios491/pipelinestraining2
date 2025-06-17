30da3947bd5d8d33b9798e6e9662b07d920361d9
DanielRiosLopez <Daniel.Rios@jala.university>
2025-06-17 00:28:58 -0400

    changes on the file postmanNeuman_commands

--- utils/postmanNeuman_commands.sh
+++ utils/postmanNeuman_commands.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+ENV=$1
+
+if [ "$ENV" = "dev" ]; then
+    PORT=5000
+elif [ "$ENV" = "prod" ]; then
+    PORT=5001
+else
+    echo "Invalid environment: $ENV"
+    exit 1
+fi
+
 # Pull Newman image
 docker pull postman/newman
 
 # Get restapp IP address
-restappIp=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' restapp)
+restappIp=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "restapp-$ENV")
 
 # Run Postman collection with newman
 docker run --rm \
     --add-host "restapp:$restappIp" \
     -v "$(pwd)/utils/tests:/etc/newman" \
     postman/newman run "/etc/newman/collectionRestAppTest.json" \
-    --environment "/etc/newman/env.json"
+    --env-var "base_url=http://restapp:$PORT"
